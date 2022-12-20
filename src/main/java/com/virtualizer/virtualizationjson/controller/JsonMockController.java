63ab26e384086fa38aa8e8aa23299d54e6f81204
Guilherme <gui.lima2304@gmail.com>
2022-12-19 22:43:32 -0300

    put

--- src/main/java/com/virtualizer/virtualizationjson/controller/JsonMockController.java
+++ src/main/java/com/virtualizer/virtualizationjson/controller/JsonMockController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class JsonMockController {
@@ -24,6 +22,16 @@ public class JsonMockController {
     public List<JsonMock> getAll() {
         List<JsonMock> jsonMockList = jsonMockRepository.findAll();
         return jsonMockList;
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<JsonMock> findJson(@PathVariable String name) {
+      JsonMock jsonFound = jsonMockRepository.findByName(name);
+        if(jsonFound != null) {
+            return ResponseEntity.ok(jsonFound);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 
--- src/main/java/com/virtualizer/virtualizationjson/repository/JsonMockRepository.java
+++ src/main/java/com/virtualizer/virtualizationjson/repository/JsonMockRepository.java
@@ -2,8 +2,12 @@ package com.virtualizer.virtualizationjson.repository;
 
 import com.virtualizer.virtualizationjson.model.JsonMock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 
 @EnableJpaRepositories
 public interface JsonMockRepository extends JpaRepository<JsonMock, Long> {
+    
+    JsonMock findByName(String name);
 }
